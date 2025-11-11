bd816de48aaa0fba273a2666635c1f45e9f30509
unknown5325 <roprawezava@tutamail.com>
2025-11-11 20:09:07 +0300

    eg

--- lion.py
+++ lion.py
@@ -44,3 +44,45 @@ data = {
 
 df = pd.DataFrame(data)
 print(df)
+
+
+
+
+doc = docx.Document('lion.docx')
+tex = ''
+for paragraph in doc.paragraphs:
+    tex += paragraph.text
+
+punc = '/?!.,"«»[](){}-–:;—_1234567890xiv'
+for i in range(0, len(punc)) :
+    if punc[i] in tex : 
+        tex = tex.replace(punc[i], ' ')
+tex = tex.lower()
+chlist = [] 
+for c in tex: 
+    chlist.append(c)
+
+ch = {}
+for item in chlist:
+    if item in ch:
+        ch[item] += 1
+    else:
+        ch[item] = 1
+
+
+
+val = []
+nuv = []
+for key in ch.keys():
+    nuv.append(ch[key])
+    val.append(key)
+#создаем график через бар, т.к. он создаёт НУЖНЫЙ график для нас, в отличии от plot
+plt.bar(val, nuv)
+
+#называем оси и сам график
+plt.xlabel("буквы")
+plt.ylabel("Количество")
+plt.title("Гистограмма количества букв")
+
+#показ графика
+plt.show()
